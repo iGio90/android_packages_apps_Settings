34b98cb0b24c524b17e44288838e4cefd78659e5
szl.kiev <szl.kiev@gmail.com>
2012-11-02 11:44:50 +0700

    15.6.0 CM stuff
    Added missing hungarian translations
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/7b4ca1b95e2ef43e14e7bcf8ffccbbd6622f8210
    
    Settings: Dutch translations
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/3f18972cf6264c28bc253879e37ba56c055fd7d0
    
    PT-BR: Added missing translations
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/8bbbc1c27ba19206ba688af849dca5dcb6396c10
    
    Settings: Updated italian translation
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/9025848a967b4ffcc4e3a331b84e351f9c6b9d10
    
    Settings: Added danish translations for SMS security limit
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/9499885a8ba7d31ba86348a8fdc751010504df61
    
    Update Russian Translation-Settings-CM10
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/a3b2e829468f8e4b0b5cdc58b6554fcc2a60720e
    
    Fixed non translatable values
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/da21d00b2936569c0e75023e7ebcb1b535b2b73e
    
    Inconsistent "Security" label size when adding a WiFi network
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/c74b7bcfd3192eff6ac73cf6d428b6b5a08f47a7
    
    settings: Add home button unlock option (2/2)
    https://github.com/ParanoidAndroid/android_packages_apps_Settings/commit/ce484c1aa825c60dfe9b6e85b098da37f8db2ef2
    
    Change-Id: Ie3e0cdc8f3811be0fda076763a8b5e6c3a8c57f0

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -77,6 +77,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private static final String SLIDE_LOCK_TIMEOUT_DELAY = "slide_lock_timeout_delay";
     private static final String SLIDE_LOCK_SCREENOFF_DELAY = "slide_lock_screenoff_delay";
     private static final String MENU_UNLOCK_PREF = "menu_unlock";
+    private static final String HOME_UNLOCK_PREF = "home_unlock";
     private static final String LOCKSCREEN_QUICK_UNLOCK_CONTROL = "quick_unlock_control";
     private static final String KEY_LOCK_BEFORE_UNLOCK = "lock_before_unlock";
     private static final String LOCK_SYNC_ENCRYPTION_PASSWORD = "lock_sync_encryption_password";
@@ -101,6 +102,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
     private CheckBoxPreference mTactileFeedback;
 
     private CheckBoxPreference mMenuUnlock;
+    private CheckBoxPreference mHomeUnlock;
     private CheckBoxPreference mQuickUnlockScreen;
     private CheckBoxPreference mShowPassword;
 
@@ -240,6 +242,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
             mMenuUnlock.setChecked(Settings.System.getInt(resolver,
                     Settings.System.MENU_UNLOCK_SCREEN, 0) == 1);
 */
+            // Home Unlock
+            mHomeUnlock = (CheckBoxPreference) root.findPreference(HOME_UNLOCK_PREF);
+            mHomeUnlock.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.HOME_UNLOCK_SCREEN, 0) == 1);
+
             // Vibrate on unlock
             mVibratePref = (CheckBoxPreference) findPreference(KEY_VIBRATE_PREF);
             mVibratePref.setChecked(Settings.System.getInt(resolver,
@@ -252,6 +259,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
                     || (mLockPatternUtils.isLockPatternEnabled())) {
                 mQuickUnlockScreen.setEnabled(false);
 //                mMenuUnlock.setEnabled(false);
+                mHomeUnlock.setEnabled(false);
                 mVibratePref.setEnabled(false);
             // disable menu unlock and vibrate on unlock options if
             // using PIN/password as primary lock screen or as
@@ -259,6 +267,7 @@ public class SecuritySettings extends SettingsPreferenceFragment
             } else if (mLockPatternUtils.isLockPasswordEnabled()) {
                 mQuickUnlockScreen.setEnabled(true);
 //                mMenuUnlock.setEnabled(false);
+                mHomeUnlock.setEnabled(false);
                 mVibratePref.setEnabled(false);
             // Disable the quick unlock if its not using PIN/password
             // as a primary lock screen or as a backup to biometric
@@ -270,6 +279,11 @@ public class SecuritySettings extends SettingsPreferenceFragment
             if (getActivity().getApplicationContext().getResources()
                     .getBoolean(com.android.internal.R.bool.config_showNavigationBar)) {
                 mMenuUnlock.setEnabled(false);
+            }
+            // Disable the HomeUnlock setting if no home button is available
+            if (getActivity().getApplicationContext().getResources()
+                    .getBoolean(com.android.internal.R.bool.config_disableHomeUnlockSetting)) {
+                mHomeUnlock.setEnabled(false);
             }
         }
 
@@ -616,6 +630,9 @@ public class SecuritySettings extends SettingsPreferenceFragment
 /*        } else if (preference == mMenuUnlock) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.MENU_UNLOCK_SCREEN, isToggled(preference) ? 1 : 0);*/
+        } else if (preference == mHomeUnlock) {
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.HOME_UNLOCK_SCREEN, isToggled(preference) ? 1 : 0);
         }  else if (preference == mVibratePref) {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.LOCKSCREEN_VIBRATE_ENABLED, isToggled(preference) ? 1 : 0);
