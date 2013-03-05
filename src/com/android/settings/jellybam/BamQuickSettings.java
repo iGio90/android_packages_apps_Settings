7ecacb1eba1dcc91b6548a7c2a830e24e64edde0
igio90 <giovanni.rocca.90@gmail.com>
2013-03-05 03:18:31 +0100

    New navring layout

--- src/com/android/settings/jellybam/BamQuickSettings.java
+++ src/com/android/settings/jellybam/BamQuickSettings.java
@@ -109,6 +109,7 @@ public class BamQuickSettings extends SettingsPreferenceFragment
     private static final String UI_EXP_WIDGET_HAPTIC_FEEDBACK = "expanded_haptic_feedback";
     private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
     private static final String PREF_NOTIFICATION_WALLPAPER_ALPHA = "notification_wallpaper_alpha";
+    private static final String PREF_NOTIFICATION_WALLPAPER = "notification_wallpaper";
     private static final String PREF_STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
     private static final String PREF_VIBRATE_NOTIF_EXPAND = "vibrate_notif_expand";
     private static final String PREF_STATUSBAR_BRIGHTNESS = "statusbar_brightness_slider";
@@ -118,6 +119,8 @@ public class BamQuickSettings extends SettingsPreferenceFragment
     private static final int SELECT_ACTIVITY = 4;
     private static final int SELECT_WALLPAPER = 5;
 
+    private static final String WALLPAPER_NAME = "notification_wallpaper.jpg";
+
     private CheckBoxPreference mPowerWidget;
     private CheckBoxPreference mPowerWidgetHideOnChange;
     private CheckBoxPreference mPowerWidgetHideScrollBar;
@@ -125,6 +128,7 @@ public class BamQuickSettings extends SettingsPreferenceFragment
 
     Preference mCustomLabel;
     Preference mWallpaperAlpha;
+    Preference mNotificationWallpaper;
 
     CheckBoxPreference mStatusBarNotifCount;
     CheckBoxPreference mVibrateOnExpand;
@@ -132,6 +136,8 @@ public class BamQuickSettings extends SettingsPreferenceFragment
 
     private ListPreference mPowerWidgetHapticFeedback;
     private ListPreference mStatusBarMaxNotif;
+
+    private int seekbarProgress;
 
     private Context mContext;
     private int mAllowedLocations;
@@ -163,6 +169,8 @@ public class BamQuickSettings extends SettingsPreferenceFragment
         updateCustomLabelTextSummary();
 
         mWallpaperAlpha = (Preference) findPreference(PREF_NOTIFICATION_WALLPAPER_ALPHA);
+
+        mNotificationWallpaper = findPreference(PREF_NOTIFICATION_WALLPAPER);
 
             mStatusBarMaxNotif = (ListPreference) prefSet.findPreference(STATUS_BAR_MAX_NOTIF);
             int maxNotIcons = Settings.System.getInt(mContext.getContentResolver(),
@@ -312,8 +320,28 @@ public class BamQuickSettings extends SettingsPreferenceFragment
                     // Canceled.
                 }
             });
+        } else if (preference == mNotificationWallpaper) {
+            Display display = getActivity().getWindowManager().getDefaultDisplay();
+            int width = display.getWidth();
+            int height = display.getHeight();
 
-            alert.show();
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+            intent.setType("image/*");
+            intent.putExtra("crop", "true");
+            boolean isPortrait = getResources()
+                    .getConfiguration().orientation
+                    == Configuration.ORIENTATION_PORTRAIT;
+            intent.putExtra("aspectX", isPortrait ? width : height);
+            intent.putExtra("aspectX", isPortrait ? width : height);
+            intent.putExtra("outputX", width);
+            intent.putExtra("outputY", height);
+            intent.putExtra("scale", true);
+            intent.putExtra("scaleUpIfNeeded", true);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, getNotificationExternalUri());
+            intent.putExtra("outputFormat", Bitmap.CompressFormat.PNG.toString());
+
+            startActivityForResult(intent, REQUEST_PICK_WALLPAPER);
+            return true;
         } else if (preference == mWallpaperAlpha) {
             Resources res = getActivity().getResources();
             String cancel = res.getString(R.string.cancel);
@@ -366,6 +394,68 @@ public class BamQuickSettings extends SettingsPreferenceFragment
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
         return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.user_interface, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.remove_wallpaper:
+                File f = new File(mContext.getFilesDir(), WALLPAPER_NAME);
+                mContext.deleteFile(WALLPAPER_NAME);
+                Helpers.restartSystemUI();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private Uri getNotificationExternalUri() {
+        File dir = mContext.getExternalCacheDir();
+        File wallpaper = new File(dir, WALLPAPER_NAME);
+
+        return Uri.fromFile(wallpaper);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_PICK_WALLPAPER) {
+
+                FileOutputStream wallpaperStream = null;
+                try {
+                    wallpaperStream = mContext.openFileOutput(WALLPAPER_NAME,
+                            Context.MODE_WORLD_READABLE);
+                } catch (FileNotFoundException e) {
+                    return; // NOOOOO
+                }
+
+                Uri selectedImageUri = getNotificationExternalUri();
+                Bitmap bitmap = BitmapFactory.decodeFile(selectedImageUri.getPath());
+
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, wallpaperStream);
+                Helpers.restartSystemUI();
+            }
+        }
+    }
+
+    public void copy(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        FileOutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
     }
 
     public static class PowerWidgetChooser extends SettingsPreferenceFragment
