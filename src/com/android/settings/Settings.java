d2c120f8274f1379f9c59270d584a23e67b64b68
Lars Greiss <kufikugel@googlemail.com>
2013-03-09 23:21:30 +0100

    Settings: Add new configuration qualifier for UI change on the fly (3/3)
    last night I got an idea after 5 weeks of breaking stuff and
    doing to much complicated things. Together with mnazim we tried
    today to realize this idea. So another absolut new unique SlimBean
    feature is born.
    
    The aim we wanted at the end was to give a simple and resource friendly
    way to change the complete UI of a rom on the fly. So why not use
    the great fallback mechanism of AOSP and change/add a new
    qualifier into the AOSP core. To explain how it works.....if you want
    to add a new UI just add in /res folder of any app or framework you want
    folders with the qualifier "inverted". Eg. drawable-inverted-xhdpi
    or values-inverted or layout-port-inverted or whatever you want. Put in
    only the files you want to modify thats it.
    
    All qualifier in AOSP are following strict rules. You can read here.
    Do not forget the order of qualifier is important. The new "inverted"
    qualifier we added to the UIMode qualifier. To get more information
    about read here
    
    http://developer.android.com/guide/topics/resources/providing-resources.html
    
    So what this commit does:
    - add new qualifier "inverted"
    - support for API level 13 and higher
    - change the custom UI on the fly
    - control default UI Mode over config.xml
    - working for all apps. They just need to include folders
      with the new qualifier name
    - resource friendly....no resource wasting either programmticaly
      then source blow, because you just need to put in the files you
      want to change
    - cause of the qualifier structure compatible with every phone,
      language or whatever qualifier you want to use on top
    - 100% compatible to theme manager. If a theme misses drawables
      they are replaced with them from the current UIMode which is set.
    
    What is the benefit why not use just a theme? We started half year
    ago TRDS which was inverted gapps system base themed....means we
    morphed directly the changes into the framework. This methoed was
    on of the reasons why trds got so popular. Points for that are easy
    first the system is themed, means each theme you activate over
    theme engine works on top the themed system. So you have a base
    style and can modify more if you want. The other reason is easy
    a lot faster and resource friendly. Now this idea went to the
    rom to provide two core system based themes and user
    can do on top what they want or not without, like you have
    
    This commit just adds the support for this
    
    cheers
    
    mnazim and kufikugel
    
    Change-Id: I401dabcb3691446aa7886c8601245f2cd2db71b8
    
    Conflicts:

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -27,6 +27,7 @@ import com.android.settings.bluetooth.BluetoothEnabler;
 import com.android.settings.deviceinfo.Memory;
 import com.android.settings.fuelgauge.PowerUsageSummary;
 import com.android.settings.profiles.ProfileEnabler;
+import com.android.settings.slim.TRDSEnabler;
 import com.android.settings.vpn2.VpnSettings;
 import com.android.settings.wifi.WifiEnabler;
 
@@ -98,6 +99,7 @@ public class Settings extends PreferenceActivity
     private Header mCurrentHeader;
     private Header mParentHeader;
     private boolean mInLocalHeaderSwitch;
+    private static Switch mTRDSSwitch;
 
     // Show only these settings for restricted users
     private int[] SETTINGS_FOR_RESTRICTED = {
@@ -613,7 +615,7 @@ public class Settings extends PreferenceActivity
         private final WifiEnabler mWifiEnabler;
         private final BluetoothEnabler mBluetoothEnabler;
         private final ProfileEnabler mProfileEnabler;
-
+        private final TRDSEnabler mTRDSEnabler;
         private AuthenticatorHelper mAuthHelper;
 
         private static class HeaderViewHolder {
@@ -626,11 +628,12 @@ public class Settings extends PreferenceActivity
         private LayoutInflater mInflater;
 
         static int getHeaderType(Header header) {
-            if (header.fragment == null && header.intent == null) {
+            if (header.fragment == null && header.intent == null && header.id != R.id.holobam_settings) {
                 return HEADER_TYPE_CATEGORY;
             } else if (header.id == R.id.wifi_settings
                     || header.id == R.id.bluetooth_settings
-                    || header.id == R.id.profiles_settings) {
+                    || header.id == R.id.profiles_settings
+                    || header.id == R.id.holobam_settings) {
                 return HEADER_TYPE_SWITCH;
             } else {
                 return HEADER_TYPE_NORMAL;
@@ -675,6 +678,7 @@ public class Settings extends PreferenceActivity
             mWifiEnabler = new WifiEnabler(context, new Switch(context));
             mBluetoothEnabler = new BluetoothEnabler(context, new Switch(context));
             mProfileEnabler = new ProfileEnabler(context, new Switch(context));
+            mTRDSEnabler = new TRDSEnabler(context, new Switch(context));
         }
 
         @Override
@@ -735,6 +739,9 @@ public class Settings extends PreferenceActivity
                         mBluetoothEnabler.setSwitch(holder.switch_);
                     } else if (header.id == R.id.profiles_settings) {
                         mProfileEnabler.setSwitch(holder.switch_);
+                    } else if (header.id == R.id.holobam_settings) {
+                        mTRDSSwitch = (Switch) view.findViewById(R.id.switchWidget);
+                        mTRDSEnabler.setSwitch(holder.switch_);
                     }
                     // No break, fall through on purpose to update common fields
 
@@ -772,12 +779,14 @@ public class Settings extends PreferenceActivity
             mWifiEnabler.resume();
             mBluetoothEnabler.resume();
             mProfileEnabler.resume();
+            mTRDSEnabler.resume();
         }
 
         public void pause() {
             mWifiEnabler.pause();
             mBluetoothEnabler.pause();
             mProfileEnabler.pause();
+            mTRDSEnabler.pause();
         }
     }
 
@@ -794,6 +803,9 @@ public class Settings extends PreferenceActivity
             highlightHeader((int) mLastHeader.id);
         } else {
             mLastHeader = header;
+        }
+        if (header.id == R.id.holobam_settings) {
+            mTRDSSwitch.toggle();
         }
     }
 
