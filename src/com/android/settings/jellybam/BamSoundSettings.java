d8baf7aec5f265fe3ff6629a989145951a588aef
igio90 <giovanni.rocca.90@gmail.com>
2013-05-30 00:00:57 +0200

    Revert "BAMControl: [2/2] Power connect/disconnect notification support"
    This reverts commit 615fdeb6865b0a803f962301793ba346a3a3f3fb.

--- src/com/android/settings/jellybam/BamSoundSettings.java
+++ src/com/android/settings/jellybam/BamSoundSettings.java
@@ -1,19 +1,14 @@
 package com.android.settings.jellybam;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
-import android.os.Vibrator;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -36,15 +31,6 @@ public class BamSoundSettings extends SettingsPreferenceFragment implements
     private static final String PREF_BT_CONNECTED_ACTION = "bt_audio_mode";
     private static final String PREF_PHONE_RING_SILENCE = "phone_ring_silence";
     private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
-    private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
-    private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
-    private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
-
-    // Request code for power notification ringtone picker
-    private static final int REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE = 1;
-
-    // Used for power notification uri string if set to silent
-    private static final String POWER_NOTIFICATIONS_SILENT_URI = "silent";
 
     SharedPreferences prefs;
     ListPreference mHeadphonesPluggedAction;
@@ -52,16 +38,10 @@ public class BamSoundSettings extends SettingsPreferenceFragment implements
     ListPreference mPhoneSilent;
     ListPreference mAnnoyingNotifications;
 
-    private CheckBoxPreference mPowerSounds;
-    private CheckBoxPreference mPowerSoundsVibrate;
-    private Preference mPowerSoundsRingtone;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle(R.string.title_sound);
-
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         addPreferencesFromResource(R.xml.jellybam_sound_settings);
         PreferenceManager.setDefaultValues(mContext, R.xml.jellybam_sound_settings, true);
@@ -78,39 +58,6 @@ public class BamSoundSettings extends SettingsPreferenceFragment implements
         mPhoneSilent.setValue((prefs.getString(PREF_PHONE_RING_SILENCE, "0")));
         mPhoneSilent.setOnPreferenceChangeListener(this);
 
-        // power state change notification sounds
-        mPowerSounds = (CheckBoxPreference) findPreference(KEY_POWER_NOTIFICATIONS);
-        mPowerSounds.setChecked(Settings.Global.getInt(resolver,
-                Settings.Global.POWER_NOTIFICATIONS_ENABLED, 0) != 0);
-        mPowerSoundsVibrate = (CheckBoxPreference) findPreference(KEY_POWER_NOTIFICATIONS_VIBRATE);
-        mPowerSoundsVibrate.setChecked(Settings.Global.getInt(resolver,
-                Settings.Global.POWER_NOTIFICATIONS_VIBRATE, 0) != 0);
-        if (vibrator == null || !vibrator.hasVibrator()) {
-            removePreference(KEY_POWER_NOTIFICATIONS_VIBRATE);
-        }
-
-        mPowerSoundsRingtone = findPreference(KEY_POWER_NOTIFICATIONS_RINGTONE);
-        String currentPowerRingtonePath =
-                Settings.Global.getString(resolver, Settings.Global.POWER_NOTIFICATIONS_RINGTONE);
-
-        // set to default notification if we don't yet have one
-        if (currentPowerRingtonePath == null) {
-                currentPowerRingtonePath = Settings.System.DEFAULT_NOTIFICATION_URI.toString();
-                Settings.Global.putString(getContentResolver(),
-                        Settings.Global.POWER_NOTIFICATIONS_RINGTONE, currentPowerRingtonePath);
-        }
-        // is it silent ?
-        if (currentPowerRingtonePath.equals(POWER_NOTIFICATIONS_SILENT_URI)) {
-            mPowerSoundsRingtone.setSummary(
-                    getString(R.string.power_notifications_ringtone_silent));
-        } else {
-            final Ringtone ringtone =
-                    RingtoneManager.getRingtone(getActivity(), Uri.parse(currentPowerRingtonePath));
-            if (ringtone != null) {
-                mPowerSoundsRingtone.setSummary(ringtone.getTitle(getActivity()));
-            }
-        }
-
         if (HeadphoneService.DEBUG)
             mContext.startService(new Intent(mContext, HeadphoneService.class));
 
@@ -121,22 +68,7 @@ public class BamSoundSettings extends SettingsPreferenceFragment implements
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
             Preference preference) {
-	if (preference == mPowerSounds) {
-            Settings.Global.putInt(getContentResolver(),
-                    Settings.Global.POWER_NOTIFICATIONS_ENABLED,
-                    mPowerSounds.isChecked() ? 1 : 0);
-
-        } else if (preference == mPowerSoundsVibrate) {
-            Settings.Global.putInt(getContentResolver(),
-                    Settings.Global.POWER_NOTIFICATIONS_VIBRATE,
-                    mPowerSoundsVibrate.isChecked() ? 1 : 0);
-
-        } else if (preference == mPowerSoundsRingtone) {
-            launchNotificationSoundPicker(REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE,
-                    Settings.Global.getString(getContentResolver(),
-                            Settings.Global.POWER_NOTIFICATIONS_RINGTONE));
-	}
-        return super.onPreferenceTreeClick(preferenceScreen, preference);
+              return super.onPreferenceTreeClick(preferenceScreen, preference);
      }
 
     private void toggleFlipService() {
@@ -163,59 +95,4 @@ public class BamSoundSettings extends SettingsPreferenceFragment implements
         }
         return false;
     }
-
-    private void launchNotificationSoundPicker(int code, String currentPowerRingtonePath) {
-        final Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-
-        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE,
-                getString(R.string.power_notifications_ringtone_title));
-        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,
-                RingtoneManager.TYPE_NOTIFICATION);
-        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_DEFAULT_URI,
-                Settings.System.DEFAULT_NOTIFICATION_URI);
-        if (currentPowerRingtonePath != null &&
-                !currentPowerRingtonePath.equals(POWER_NOTIFICATIONS_SILENT_URI)) {
-            Uri uri = Uri.parse(currentPowerRingtonePath);
-            if (uri != null) {
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, uri);
-            }
-        }
-        startActivityForResult(intent, code);
-    }
-
-    private void setPowerNotificationRingtone(Intent intent) {
-        final Uri uri = intent.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
-
-        final String toneName;
-        final String toneUriPath;
-
-        if ( uri != null ) {
-            final Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), uri);
-            toneName = ringtone.getTitle(getActivity());
-            toneUriPath = uri.toString();
-        } else {
-            // silent
-            toneName = getString(R.string.power_notifications_ringtone_silent);
-            toneUriPath = POWER_NOTIFICATIONS_SILENT_URI;
-        }
-
-        mPowerSoundsRingtone.setSummary(toneName);
-        Settings.Global.putString(getContentResolver(),
-                Settings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriPath);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE:
-                if (resultCode == Activity.RESULT_OK) {
-                    setPowerNotificationRingtone(data);
-                }
-                break;
-            default:
-                super.onActivityResult(requestCode, resultCode, data);
-                break;
-        }
-    }
-
 }
