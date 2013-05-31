b6c9d3a9ad246172a9840ac5588d6cd0cfb720b2
igio90 <giovanni.rocca.90@gmail.com>
2013-05-31 23:49:58 +0200

    Pie: Add option to change pie's angle from the attach 1/2
    Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- src/com/android/settings/paranoid/Pie.java
+++ src/com/android/settings/paranoid/Pie.java
@@ -45,6 +45,7 @@ public class Pie extends SettingsPreferenceFragment
     private static final String PIE_MODE = "pie_mode";
     private static final String PIE_SIZE = "pie_size";
     private static final String PIE_TRIGGER = "pie_trigger";
+    private static final String PIE_ANGLE = "pie_angle";
     private static final String PIE_GAP = "pie_gap";
     private static final String PIE_CENTER = "pie_center";
     private static final String PIE_STICK = "pie_stick";
@@ -58,6 +59,7 @@ public class Pie extends SettingsPreferenceFragment
     private ListPreference mPieSize;
     private ListPreference mPieGravity;
     private ListPreference mPieTrigger;
+    private ListPreference mPieAngle;
     private ListPreference mPieGap;
     private CheckBoxPreference mPieCenter;
     private CheckBoxPreference mPieStick;
@@ -129,6 +131,12 @@ public class Pie extends SettingsPreferenceFragment
                 Settings.System.PIE_GAP, 3);
         mPieGap.setValue(String.valueOf(pieGap));
         mPieGap.setOnPreferenceChangeListener(this);
+
+        mPieAngle = (ListPreference) prefSet.findPreference(PIE_ANGLE);
+        int pieAngle = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.PIE_ANGLE, 0);
+        mPieAngle.setValue(String.valueOf(pieAngle));
+        mPieAngle.setOnPreferenceChangeListener(this);
 
         mPieNotifi = (CheckBoxPreference) prefSet.findPreference(PIE_NOTIFICATIONS);
         mPieNotifi.setChecked((Settings.System.getInt(getContentResolver(),
@@ -206,6 +214,11 @@ public class Pie extends SettingsPreferenceFragment
             int pieGravity = Integer.valueOf((String) newValue);
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.PIE_GRAVITY, pieGravity);
+            return true;
+        } else if (preference == mPieAngle) {
+            int pieAngle = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.PIE_ANGLE, pieAngle);
             return true;
         } else if (preference == mPieGap) {
             int pieGap = Integer.valueOf((String) newValue);
