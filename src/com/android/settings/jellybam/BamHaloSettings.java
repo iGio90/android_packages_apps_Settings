a8705a90020f30e3162bd2f59531e388c4ccbae5
igio90 <giovanni.rocca.90@gmail.com>
2013-06-11 03:41:07 +0200

    BamControl: HALO! Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- src/com/android/settings/jellybam/BamHaloSettings.java
+++ src/com/android/settings/jellybam/BamHaloSettings.java
@@ -1,11 +1,14 @@
 package com.android.settings.jellybam;
 
 import android.app.AlertDialog;
+import android.app.INotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -14,6 +17,7 @@ import android.preference.PreferenceScreen;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.view.IWindowManager;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -22,11 +26,15 @@ import com.android.settings.Utils;
 public class BamHaloSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_HALO_STATE = "halo_state";
     private static final String KEY_HALO_HIDE = "halo_hide";
     private static final String KEY_HALO_REVERSED = "halo_reversed";
 
+    private ListPreference mHaloState;
     private CheckBoxPreference mHaloHide;
     private CheckBoxPreference mHaloReversed;
+
+    private INotificationManager mNotificationManager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,13 @@ public class BamHaloSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.jellybam_halo_settings);
 
 	PreferenceScreen prefSet = getPreferenceScreen();
+
+        mNotificationManager = INotificationManager.Stub.asInterface(
+                ServiceManager.getService(Context.NOTIFICATION_SERVICE));
+
+        mHaloState = (ListPreference) prefSet.findPreference(KEY_HALO_STATE);
+        mHaloState.setValue(String.valueOf((isHaloPolicyBlack() ? "1" : "0")));
+        mHaloState.setOnPreferenceChangeListener(this);
 
         mHaloHide = (CheckBoxPreference) prefSet.findPreference(KEY_HALO_HIDE);
         mHaloHide.setChecked(Settings.System.getInt(mContext.getContentResolver(),
@@ -47,9 +62,17 @@ public class BamHaloSettings extends SettingsPreferenceFragment implements
 
     }
 
-    @Override
+   private boolean isHaloPolicyBlack() {
+        try {
+            return mNotificationManager.isHaloPolicyBlack();
+        } catch (android.os.RemoteException ex) {
+                // System dead
+        }
+        return true;
+    }
+
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen,
-            Preference preference) {
+            Preference preference, Object newValue) {
 	if (preference == mHaloHide) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_HIDE, mHaloHide.isChecked()
@@ -58,7 +81,15 @@ public class BamHaloSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HALO_REVERSED, mHaloReversed.isChecked()
                      ? 1 : 0);
-        }
+        } else if (preference == mHaloState) {
+            boolean state = Integer.valueOf((String) newValue) == 1;
+            try {
+                mNotificationManager.setHaloPolicyBlack(state);
+            } catch (android.os.RemoteException ex) {
+                // System dead
+            }
+            return true;
+	}
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
