d8e7b64f47e8d795ed9884b224e98051eb4d9093
igio90 <giovanni.rocca.90@gmail.com>
2013-06-18 12:49:10 +0200

    BIG DERP Signed-off-by: igio90 <giovanni.rocca.90@gmail.com>

--- src/com/android/settings/jellybam/BamHaloSettings.java
+++ src/com/android/settings/jellybam/BamHaloSettings.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.paranoid;
+package com.android.settings.jellybam;
 
 import android.app.ActivityManager;
 import android.app.AlertDialog;
